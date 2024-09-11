bb9eb88f87b91c7e5298fded8ed30c254841cf26
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-11 12:44:13 +0530

    created get method on home route

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 
 // port should be inside env
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // listen method is used to create server
 // it takes port as an argument
-app.listen(3000, () => {
-  console.log("listening on port:", 3000);
+app.listen(PORT, () => {
+  console.log("listening on port:", PORT);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello from server!")
 });
