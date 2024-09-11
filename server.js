d70eeae2225a112035858c1d21d288baca730ab4
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-11 17:18:26 +0530

    check static file use in express

--- server.js
+++ server.js
@@ -22,17 +22,20 @@ app.listen(PORT, () => {
 // });
 
 // Response can be HTML - external file
-app.get("/", (req, res) => {
-    // res.send(path.resolve(__dirname) + "/index.html");
+// app.get("/", (req, res) => {
+//     // res.send(path.resolve(__dirname) + "/index.html");
 
-    res.sendFile(path.resolve(__dirname) + "/index.html");
-  });
+//     res.sendFile(path.resolve(__dirname) + "/index.html");
+//   });
 
 //path.resolve(__dirname) -> gives current directories path
 //res.send -> to send response
 //res.sendFile -> to send file as response
 
 // ADD Multiple routes
-app.get("/about", (req, res) => {
-    res.sendFile(path.resolve(__dirname) + "/about.html");
-  });
+// app.get("/about", (req, res) => {
+//     res.sendFile(path.resolve(__dirname) + "/about.html");
+//   });
+
+// static files like these static html pages can be added using app.use, it requires static file directory
+app.use(express.static('public'))
